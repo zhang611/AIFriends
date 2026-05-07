01a91d3a6444d92fcd50698bf77609977683e97f
zzc <zhangzhichao0601@163.com>
2026-05-07 22:38:29 +0800

    feat:创建导航栏

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,15 @@ import {fileURLToPath, URL} from 'node:url'
 import path from 'path'
 
 import {defineConfig} from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         vue(),
         vueDevTools(),
     ],
